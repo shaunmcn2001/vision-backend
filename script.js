e76c3a714b2e683657e463542bd4cdd8a36a7ef5
shaunmcn2001 <stmcneilly@gmail.com>
2025-07-13 15:56:05 +1000

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('toggleResultsBtn').addEventListener('click', function() {
+    var resultsPanel = document.getElementById('resultsPanel');
+    resultsPanel.classList.toggle('show');
+});
